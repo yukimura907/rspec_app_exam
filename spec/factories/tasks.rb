5917fd6f308a4a7df19813f7210806b3336f1cc8
test <test@tesutonoMacBook-Air.local>
2020-12-12 23:58:26 +0900

    revise:システムスペックの課題要件を全て達成

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -5,5 +5,10 @@ FactoryBot.define do
     from = Date.parse("2019/08/01")
     to   = Date.parse("2019/12/31")
     deadline { Random.rand(from..to) }
+
+    trait :done do
+      status { :done }
+      completion_date { Time.current.yesterday }
+    end
   end
 end
